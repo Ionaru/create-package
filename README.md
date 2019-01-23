dcbda0893d697396a532386d3695824d4b4cb967
Jeroen Akkerman <jeroen_akkerman1@hotmail.com>
2019-01-23 17:53:27 +0100

    Add project files

---
+++ README.md
@@ -0,0 +1,6 @@
+# @ionaru/create-package (UNFINISHED)
+
+## Usage
+```
+npx @ionaru/create-package
+```
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log('hi');
+
+function doSomething() {
+    console.log('Doing something!');
+}
+
+doSomething();
